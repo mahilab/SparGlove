5d557ba0a38fb2e622668d6988c357ff2e5ebe2a
John E Britt <jebaroo@ad.rice.edu>
2019-02-11 00:55:14 -0600

    All the files necessary to classify MYO data from a file

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,12 @@
 #include <MEL/Mechatronics/VelocitySensor.hpp>
 #include <MEL/Core/Console.hpp>
 #include <MEL/Logging/Log.hpp>
-#include "SparGlove.hpp"
+
+
+//#include "SparGlove.hpp"
+
+
+
 #include <MEL/Devices/Windows/Keyboard.hpp>
 #include "MYO_Classifier.hpp"
 
@@ -35,64 +40,64 @@ return 0;
 // MAIN
 //==============================================================================
 
-
-int GloveDriver(int MYO) {
-
-	// register CTRL-C handler
-	// make and parse console options
-	/*Options options("spar_glove.exe", "Spar Glove demo");
-	options.add_options()
-	("o,home", "Homing")
-	("h,help", "Prints this Help Message");*/
-	//auto input = options.parse(argc, argv);
-
-	// print help message if requested
-
-	// create Spar Glove instance
-	SparGlove sg;
-
-	// enable Spar Glove
-	prompt("Press ENTER to enable SPAR Glove");
-	sg.enable();
-
-	print("Enabled the Spar Glove");
-
-	//sg.experiment1(stop);
-
-	// disable spare glove
-	sg.disable();
-
-	return 0;
-}
+//
+//int GloveDriver(int MYO) {
+//
+//	// register CTRL-C handler
+//	// make and parse console options
+//	/*Options options("spar_glove.exe", "Spar Glove demo");
+//	options.add_options()
+//	("o,home", "Homing")
+//	("h,help", "Prints this Help Message");*/
+//	//auto input = options.parse(argc, argv);
+//
+//	// print help message if requested
+//
+//	// create Spar Glove instance
+//	SparGlove sg;
+//
+//	// enable Spar Glove
+//	prompt("Press ENTER to enable SPAR Glove");
+//	sg.enable();
+//
+//	print("Enabled the Spar Glove");
+//
+//	//sg.experiment1(stop);
+//
+//	// disable spare glove
+//	sg.disable();
+//
+//	return 0;
+//}
 
 //	std::thread myThread(threadFunction);
 //std::thread IntentDetection(MYOClassifier);
 
 
-int main1() {
-	std::vector <std::string> v;
-	v = {};// "Fist_Training.csv", "PointTraining.csv", "ButtonTraining.csv", "ExtensionTraining.csv", "OKTraining.csv", "ThumbOppoTraining.csv"};
-
-	//MYOClassifier mc = MYOClassifier(v);
-	SparGlove sg;
-
-	sg.on_enable();
-	sg.zero_encoders();
-
-	sg.on_enable();
-	sg.start_myo();
-	//sg.start_turning();
-	
-
-	return 0;
-
-}
+//int main1() {
+//	std::vector <std::string> v;
+//	v = {};// "Fist_Training.csv", "PointTraining.csv", "ButtonTraining.csv", "ExtensionTraining.csv", "OKTraining.csv", "ThumbOppoTraining.csv"};
+//
+//	//MYOClassifier mc = MYOClassifier(v);
+//	SparGlove sg;
+//
+//	sg.on_enable();
+//	sg.zero_encoders();
+//
+//	sg.on_enable();
+//	sg.start_myo();
+//	//sg.start_turning();
+//	
+//
+//	return 0;
+//
+//}
 
 int main() {
 	std::vector <std::string> v;
 	v = { "PoseButtonTraining.csv", "PoseExtensionTraining.csv", "PoseOKTraining.csv", "PosePointTraining.csv", "PoseCylinderTraining.csv" ,  "PoseThumbOppoTraining.csv", "PoseThumbsUpTraining.csv" };
 
-	SparGlove sg;
+	//SparGlove sg;
 
 	//sg.enable();
 
@@ -100,31 +105,31 @@ int main() {
 
 	return 0;
 }
-
-int main2() {
-	Q8Usb q8;
-	q8.open();
-	q8.AO.set_disable_values(std::vector<Voltage>(8, 0.0)); // default is 0.0
-	q8.AO.set_expire_values(std::vector<Voltage>(8, 0.0));  // default is 0.0
-	q8.enable();
-
-	q8.AO[0].set_value(0.9);
-
-	Timer timer(hertz(1000));
-
-	q8.watchdog.set_timeout(milliseconds(100));
-	q8.watchdog.start();
-
-	while (true) {
-		q8.update_input();
-		q8.watchdog.kick();
-		if (Keyboard::is_key_pressed(Key::Space))
-			break;
-		q8.update_output();
-		timer.wait();
-	}
-
-	q8.disable();
-	q8.close();
-	return 0;
-}
+//
+//int main2() {
+//	Q8Usb q8;
+//	q8.open();
+//	q8.AO.set_disable_values(std::vector<Voltage>(8, 0.0)); // default is 0.0
+//	q8.AO.set_expire_values(std::vector<Voltage>(8, 0.0));  // default is 0.0
+//	q8.enable();
+//
+//	q8.AO[0].set_value(0.9);
+//
+//	Timer timer(hertz(1000));
+//
+//	q8.watchdog.set_timeout(milliseconds(100));
+//	q8.watchdog.start();
+//
+//	while (true) {
+//		q8.update_input();
+//		q8.watchdog.kick();
+//		if (Keyboard::is_key_pressed(Key::Space))
+//			break;
+//		q8.update_output();
+//		timer.wait();
+//	}
+//
+//	q8.disable();
+//	q8.close();
+//	return 0;
+//}
